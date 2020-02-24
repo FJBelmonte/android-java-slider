4e24740d8966cc6cc52d5d56400110d4000c0422
Fernando Belmonte <fernando.belmonte@live.com>
2020-02-23 22:07:03 -0300

    Initial commit

---
+++ app/src/main/java/com/coffeit/slider/MainActivity.java
@@ -0,0 +1,47 @@
+package com.coffeit.slider;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 1")
+                .description("Descrição 1")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .scrollable(false)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 2")
+                .description("Descrição 2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .scrollable(false)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 3")
+                .description("Descrição 3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .scrollable(false)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 4")
+                .description("Descrição 4")
+                .image(R.drawable.quatro)
+                .background(android.R.color.holo_orange_light)
+                .scrollable(false)
+                .build());
+    }
+}
